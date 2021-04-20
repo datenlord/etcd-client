b1d6a0c47a46783e1a1ed42af0beaed3d2a6205d
Roger Shi <rogershijicheng@gmail.com>
2021-04-20 10:50:26 +0800

    Remove dependecy of async-std caused by the backoff lib (#20)
    Use the feature futures instead of async-std
    
    Co-authored-by: Jicheng Shi <jicheng.shi@datenlord.io>

--- src/lease/mod.rs
+++ src/lease/mod.rs
@@ -83,7 +83,6 @@ use crate::INITIAL_INTERVAL_ENV_KEY;
 use crate::INITIAL_INTERVAL_VALUE;
 use crate::MAX_ELAPSED_TIME_ENV_KEY;
 use crate::MAX_ELAPSED_TIME_VALUE;
-use backoff::future::retry;
 use backoff::ExponentialBackoff;
 use std::time::Duration;
 
